73564a3a01a802c7dc6763def715597cf1583bf6
schperplata <domen.jurkovic@gmail.com>
2014-09-11 16:39:01 +0200

    Update main.c

--- USART/EXAMPLE/main.c
+++ USART/EXAMPLE/main.c
@@ -27,6 +27,40 @@
 ////send raw data, any type.
 //void writeData(void *data, uint8_t dataSize);
 
+//STM32L100RCT6-Discovery - clock correction function - SYSCLK = 32MHz, HSI. 
+void clockCorrection()
+{
+	RCC_DeInit();
+	RCC_HSICmd(ENABLE);
+	
+	while(RCC_GetFlagStatus(RCC_FLAG_HSIRDY) != SET)	// Wait until HSI is ready
+	{
+    }
+	 
+	FLASH->ACR |= FLASH_ACR_ACC64;		// Enable 64-bit access 
+    FLASH->ACR |= FLASH_ACR_PRFTEN;		// Enable Prefetch Buffer 
+	FLASH->ACR |= FLASH_ACR_LATENCY;	// Flash 1 wait state 
+    
+	RCC->APB1ENR |= RCC_APB1ENR_PWREN;	// Power enable
+    PWR->CR = PWR_CR_VOS_0;				// Select the Voltage Range 1 (1.8 V) 
+    while((PWR->CSR & PWR_CSR_VOSF) != RESET) // Wait until the Voltage Regulator is ready 
+    {
+    }
+	  
+	RCC_PLLConfig(RCC_PLLSource_HSI, RCC_PLLMul_4, RCC_PLLDiv_2);	// HSI = 16MHz; 16MHz * 4 / 2 = 32MHz
+	RCC_PLLCmd(ENABLE);
+	while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) != SET)	// Wait untill PLL is ready
+	{
+    }
+	RCC_PCLK1Config(RCC_HCLK_Div1);			// Configures the Low Speed APB(APB1) clock (PCLK1).
+	RCC_PCLK2Config(RCC_HCLK_Div1);			// Configures the High Speed APB(APB2) clock (PCLK2).
+
+	RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);	// Configures the System Clock source to PLL
+	while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) != (uint32_t)RCC_CFGR_SWS_PLL)	// Wait till PLL is used as system clock source 
+    {
+    }
+}
+
 int main(void)
 {
 	uint16_t character = 0;
